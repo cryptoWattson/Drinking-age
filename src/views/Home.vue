cc62054ec97f5b4043780de9cbfeba93dbe0d8c8
crytpoWattson <isaaclibis@gmail.com>
2022-04-06 13:16:19 -0400

    the timer code. needs review!!

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <counter
+    : year ="2020"
+    :month-"4"
+    :date-"1"
+    :hour-"14"
+    :minute-"23"
+    :second-"59"
+    :millisecond-"59"
+    />
+  </div>
+</template>
+
+<script>
+const { default: counter }=require("@/components/counter.vue")
+
+export defa
+  components: { counter },ult {
+
+};
+</script>
+
+<style scoped>
+
+</style>
